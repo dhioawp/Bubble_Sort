8ceeea54b76a74e8a3fb915b39f79d678f246441
Lenovo <Lenovo@LAPTOP-CKBECVBP>
2022-10-17 17:07:05 +0700

    Menambahkan Method

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -10,5 +10,33 @@
         //number of elements in array
         private int n;
 
+
+
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elemenets in the array :");
+                string s = Console.ReadLine();
+                n = int.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }    
+
+        }
+
     }
 }

72670edb46375b13b0e07616ac49c48c20832b4f
Lenovo <Lenovo@LAPTOP-CKBECVBP>
2022-10-17 17:43:00 +0700

    Membuat method q_sort menggunakan algorithma quick sort

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -65,6 +65,47 @@ namespace QuickSort
             i = low + 1;
             j = high;
 
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greather than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] <= pivot) && (j <= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the geather element is on the left of the element
+                {
+                    //swap the element at index i with the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+            //j low contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+                
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);  
         }
     }
 }

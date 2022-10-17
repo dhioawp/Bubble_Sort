7dc88aada787b8505096d3b31b29a07d44465322
Lenovo <Lenovo@LAPTOP-CKBECVBP>
2022-10-17 16:56:42 +0700

    Membuat namespace dan class program

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace QuickSort
+{
+    class program
+    {
+        //Array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparassion
+        private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
+
+    }
+}
